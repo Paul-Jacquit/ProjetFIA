f4b1d40f102d9db1f0ba395ad3c43dcfe1babf7a
Anthony <anthonylabat@orange.fr>
2020-01-30 11:52:42 +0100

    Création de l'entité, de la branche, et de la premiere page html

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,10 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'contact-information',
+        loadChildren: () => import('./contact-information/contact-information.module').then(m => m.ProjetFiaContactInformationModule)
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ]
